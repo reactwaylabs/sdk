400f48e646172e8f69b15b04318ce4670578c518
Martynas Zilinskas <martynas@quatrodev.com>
2020-12-01 00:43:59 +0200

    Removed examples project.

--- packages/examples/hello-world/.babelrc.js
+++
@@ -1,7 +0,0 @@
-const isDevelopment = process.env.NODE_ENV == "development";
-
-const devPlugins = ["react-refresh/babel"];
-
-module.exports = {
-    plugins: [...(isDevelopment ? devPlugins : [])]
-};
--- packages/examples/hello-world/src/test.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import "./test.css";
-
-export const Test = () => {
-    return <div className="test">Test</div>;
-};
